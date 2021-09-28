019a4f137d477c81fbc6606cd5d57cb6030f513e
nishantgrg21 <nishantgrg21@gmail.com>
2021-09-28 20:21:34 +0530

    UI Changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout/layout.component';
+import { CollectionsComponent } from './pages/collections/collections.component';
+import { CompaignComponent } from './pages/compaign/compaign.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ProductsComponent } from './pages/products/products.component';
 
 const routes: Routes = [
   {
@@ -11,12 +14,24 @@ const routes: Routes = [
     children:
     [
       {
-        path:'dashboard',
+        path:'',
         component:DashboardComponent
       },
       {
         path:'home',
         component:HomeComponent
+      },
+      {
+        path:'collections',
+        component:CollectionsComponent
+      },
+      {
+        path:'compaign',
+        component:CompaignComponent
+      },
+      {
+        path:'products',
+        component:ProductsComponent
       },
     ]
   }
